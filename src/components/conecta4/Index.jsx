af81b228cd076791d75324aef0bc564eaa0afd7c
Luis <lm.perezpacheco@gmail.com>
2025-12-17 17:05:56 +0100

    fix error

--- src/components/conecta4/Index.jsx
+++ src/components/conecta4/Index.jsx
@@ -1,6 +1,6 @@
 import { Board } from "./Board";
 
 
-export default function CONECTA_4 ({changeGame}) {
+export function CONECTA_4 ({changeGame}) {
     return(<div className="conecta4"><Board changeGame={changeGame}/></div>)
 }
--- src/components/tres-en-raya/Index.jsx
+++ src/components/tres-en-raya/Index.jsx
@@ -1,5 +1,5 @@
 import { Board } from "./Board";
 
-export default function TRES_EN_RAYA ({changeGame}){
+export function TRES_EN_RAYA ({changeGame}){
     return(<div className="tres-en-raya"><Board changeGame={changeGame}/></div>)
 }
